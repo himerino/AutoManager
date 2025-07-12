887d7310f3b63fb2a808215f595cf5d1ffe56127
Ray <rayshelldiaz@gmail.com>
2025-07-12 15:31:44 -0500

    Agregada funcionalidad de proveedores: vista, controlador y conexión en vista principal

--- src/main/java/automanager/controlador/ControladorProveedor.java
+++ src/main/java/automanager/controlador/ControladorProveedor.java
@@ -1,5 +1,36 @@
 package automanager.controlador;
 
+import automanager.Sistema;
+import automanager.modelo.Proveedor;
+import automanager.vista.MensajeUsuario;
+
+import java.util.ArrayList;
+
 public class ControladorProveedor {
 
+    private Sistema sistema;
+
+    public ControladorProveedor(Sistema sistema) {
+        this.sistema = sistema;
+    }
+
+    public MensajeUsuario agregarProveedor(String id, String nombre, String telefono, String descripcion) {
+        if (buscarProveedorPorId(id) != null) {
+            return new MensajeUsuario("Agregar Proveedor", "Ya existe un proveedor con esa identificación.");
+        }
+        Proveedor proveedor = new Proveedor(id, nombre, telefono, descripcion);
+        sistema.getProveedores().add(proveedor);
+        return null;
+    }
+
+    public Proveedor buscarProveedorPorId(String id) {
+        for (Proveedor p : sistema.getProveedores()) {
+            if (p.getId().equals(id)) return p;
+        }
+        return null;
+    }
+
+    public ArrayList<Proveedor> getListaProveedores() {
+        return sistema.getProveedores();
+    }
 }
